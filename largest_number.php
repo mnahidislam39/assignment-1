57623ff77eb7750213710f874234bae3ff605bf4
devnknahid <dev.nknahid@gmail.com>
2024-07-03 20:28:18 +0600

    is big

--- largest_number.php
+++ largest_number.php
@@ -4,9 +4,9 @@ $num1 = 4;
 $num2 = 5;
 $num3 = 6;
 
-if ($num1 >= $num2 && $num1 >= $num3) {
+if ($num1 > $num2 && $num1 > $num3) {
     printf("The largest number is: $num1");
-} elseif ($num2 >= $num1 && $num2 >= $num3) {
+} elseif ($num2 > $num1 && $num2 > $num3) {
     printf("The largest number is: $num2");
 } else {
     printf("The largest number is: $num3");
